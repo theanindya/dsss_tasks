9a5b1991e8814f217b311557465eba523855a506
theanindya <40605723+theanindya@users.noreply.github.com>
2023-11-13 21:45:28 +0100

    Update tests_math_quiz.py

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,31 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from unittest.mock import patch
+from io import StringIO
+from your_module_name import math_quiznew  # replace 'your_module_name' with the actual module name
 
 
-class TestMathGame(unittest.TestCase):
+class TestMathQuiz(unittest.TestCase):
+    @patch("builtins.input", side_effect=["5", "2", "8"])
+    def test_math_quiz_correct_answer(self, mock_input):
+        with patch("sys.stdout", new_callable=StringIO) as mock_stdout:
+            math_quiznew()
+        output = mock_stdout.getvalue().strip()
+        self.assertIn("Correct! You earned a point.", output)
 
-    def test_function_A(self):
-        # Test if random numbers generated are within the specified range
-        min_val = 1
-        max_val = 10
-        for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
-            self.assertTrue(min_val <= rand_num <= max_val)
+    @patch("builtins.input", side_effect=["5", "2", "7"])
+    def test_math_quiz_wrong_answer(self, mock_input):
+        with patch("sys.stdout", new_callable=StringIO) as mock_stdout:
+            math_quiznew()
+        output = mock_stdout.getvalue().strip()
+        self.assertIn("Wrong answer. The correct answer is", output)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    @patch("builtins.input", side_effect=["abc", "5", "2"])
+    def test_math_quiz_invalid_input(self, mock_input):
+        with patch("sys.stdout", new_callable=StringIO) as mock_stdout:
+            math_quiznew()
+        output = mock_stdout.getvalue().strip()
+        self.assertIn("Invalid input. Please enter a valid integer.", output)
 
-    def test_function_C(self):
-            test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
-            ]
-
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
 
 if __name__ == "__main__":
     unittest.main()
