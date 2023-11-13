98e9da0b4ba5493cdea60c9ba56a3f5d17f037b1
theanindya <40605723+theanindya@users.noreply.github.com>
2023-11-13 21:50:47 +0100

    Create setup.py
    setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+"""Setup File necessary for Python Packaging"""
+from setuptools import find_packages, setup
+
+setup(
+    name="math_quiz",
+    version="1.0",
+    author="Anindya Banerjee (so50wotu)",
+    author_email="example@xyz.com",
+    packages=find_packages(),
+)
